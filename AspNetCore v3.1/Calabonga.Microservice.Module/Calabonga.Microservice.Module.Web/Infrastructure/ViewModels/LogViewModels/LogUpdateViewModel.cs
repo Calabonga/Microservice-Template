ca60967ae7a3c9e81e1af2e5c87de5ebe8badc79
Calabonga <calabonga@outlook.com>
2019-12-13 18:35:50 +0700

    Module Project created

---
+++ AspNetCore v3.1/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/Infrastructure/ViewModels/LogViewModels/LogUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Calabonga.EntityFrameworkCore.Entities.Base;
+
+namespace Calabonga.Microservice.Module.Web.Infrastructure.ViewModels.LogViewModels
+{
+    /// <summary>
+    /// Log View model for Update operations
+    /// </summary>
+    public class LogUpdateViewModel : ViewModelBase
+    {
+        public string Logger { get; set; }
+
+        public string Level { get; set; }
+
+        public string Message { get; set; }
+    }
+}
