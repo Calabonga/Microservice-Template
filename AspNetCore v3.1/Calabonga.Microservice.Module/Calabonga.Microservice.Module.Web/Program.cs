4c2d714f4f2b3a6576e767af7d40321096fc7927
Calabonga <dev@calabonga.net>
2020-07-22 10:07:37 +0700

    Template without IdentityServer4 updated. Zip created. Old removed

--- AspNetCore v3.1/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/Program.cs
+++ AspNetCore v3.1/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/Program.cs
@@ -1,10 +1,10 @@
 // ---------------------------------------
 // Name: Microservice Template
 // Author: Calabonga (calabonga.net)
-// Version: 2.0.2
+// Version: 2.0.3
 // Based on: ASP.NET Core 3.1
 // Created Date: 2019-10-06
-// Updated Date 2020-05-25
+// Updated Date 2020-07-22
 // ---------------------------------------
 
 using System;
