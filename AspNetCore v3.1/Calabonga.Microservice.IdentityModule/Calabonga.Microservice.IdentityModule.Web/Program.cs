dacdc258b27f20a979ca9d25c1efc370becfbb01
Calabonga <calabonga@outlook.com>
2020-09-04 08:15:25 +0700

    version 2.0.4

--- AspNetCore v3.1/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Program.cs
+++ AspNetCore v3.1/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Program.cs
@@ -1,10 +1,11 @@
 // ---------------------------------------
 // Name: Microservice Template
 // Author: Calabonga (calabonga.net)
-// Version: 2.0.3
+// Telegram: t.me/calabongaNet
+// Version: 2.0.4   
 // Based on: ASP.NET Core 3.1
 // Created Date: 2019-10-06
-// Updated Date 2020-07-22
+// Updated Date 2020-09-04
 // ---------------------------------------
 
 using System;
