01250014daf1fa59ae4cf634912ebb60fd59594d
Calabonga <calabonga@outlook.com>
2020-09-16 17:06:08 +0700

    Descriptions updated

--- AspNetCore v3.1/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/AppStart/ConfigureServices/ConfigureServicesCommon.cs
+++ AspNetCore v3.1/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/AppStart/ConfigureServices/ConfigureServicesCommon.cs
@@ -24,12 +24,12 @@ namespace Calabonga.Microservice.Module.Web.AppStart.ConfigureServices
         {
             services.AddDbContextPool<ApplicationDbContext>(config =>
             {
-                // This for demo only.
-                // Should uninstall package "Microsoft.EntityFrameworkCore.InMemory" and
+                // UseInMemoryDatabase - This for demo purposes only!
+                // Should uninstall package "Microsoft.EntityFrameworkCore.InMemory" and install what you need. 
+                // For example: "Microsoft.EntityFrameworkCore.SqlServer"
                 // uncomment line below to use UseSqlServer(). Don't forget setup connection string in appSettings.json 
                 config.UseInMemoryDatabase("DEMO_PURPOSES_ONLY");
-
-                //config.UseSqlServer(configuration.GetConnectionString(nameof(ApplicationDbContext)));
+                // config.UseSqlServer(configuration.GetConnectionString(nameof(ApplicationDbContext)));
             });
 
             services.AddAutoMapper(typeof(Startup));
