26622e78a0f0c2b738aa0aaa2a1a0869e7ca339a
Calabonga <calabonga@outlook.com>
2020-12-03 16:56:01 +0700

    UI Helpers for IdentityServer4 added

---
+++ IdentityServer4Mvc/ViewModels/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calabonga.Microservice.IdentityModule.Web.ViewModels.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ReturnUrl { get; set; }
+    }
+}
