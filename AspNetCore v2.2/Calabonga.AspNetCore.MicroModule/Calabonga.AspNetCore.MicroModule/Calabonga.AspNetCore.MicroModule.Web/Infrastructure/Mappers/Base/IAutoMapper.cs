9c3f4a8b80eac2aa793d2133de438e2af39318e6
Calabonga <dev@calabonga.net>
2021-11-09 19:46:10 +0700

    Old .NET version were removed
    v2.2 and v3.0 is not required any more

--- AspNetCore v2.2/Calabonga.AspNetCore.MicroModule/Calabonga.AspNetCore.MicroModule/Calabonga.AspNetCore.MicroModule.Web/Infrastructure/Mappers/Base/IAutoMapper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Calabonga.AspNetCore.MicroModule.Web.Infrastructure.Mappers.Base
-{
-    /// <summary>
-    /// The stub for reflection helper profiles registrations
-    /// </summary>
-    public interface IAutoMapper { }
-}
