0db02f170cdc0e2183207b4b7c90196bd362aa56
Calabonga <dev@calabonga.net>
2022-05-07 12:47:46 +0700

    OpenIddict in progress...

--- AspNetCore v6.0/OpenIddictMigration/Calabonga.AuthService/Calabonga.AuthService.Web/Endpoints/AuthEndpoints/TokenEndpoints.cs
+++ AspNetCore v6.0/OpenIddictMigration/Calabonga.AuthService/Calabonga.AuthService.Web/Endpoints/AuthEndpoints/TokenEndpoints.cs
@@ -1,94 +1,44 @@
-﻿using Calabonga.AuthService.Web.Definitions.Base;
+﻿using Calabonga.AuthService.Domain.Base;
+using Calabonga.AuthService.Web.Definitions.Base;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Mvc;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
 using System.Security.Claims;
 
-namespace Calabonga.AuthService.Web.Endpoints.AuthEndpoints
+namespace Calabonga.AuthService.Web.Endpoints.AuthEndpoints;
+
+public class TokenEndpoints : AppDefinition
 {
-    public class TokenEndpoints : AppDefinition
+    public override void ConfigureApplication(WebApplication app, IWebHostEnvironment environment) =>
+        app.MapPost("~/connect/token", TokenAsync);
+
+    private IResult TokenAsync(HttpContext httpContext, IOpenIddictScopeManager manager)
     {
-        public override void ConfigureApplication(WebApplication app, IWebHostEnvironment environment)
+        var request = httpContext.GetOpenIddictServerRequest() ?? throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+        ClaimsPrincipal claimsPrincipal;
+
+        if (request.IsClientCredentialsGrantType())
         {
-            app.MapPost("~/connect/token", Exchange);
+            var identity = new ClaimsIdentity(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+            // Subject or sub is a required field, we use the client id as the subject identifier here.
+            identity.AddClaim(OpenIddictConstants.Claims.Subject, AppData.ServiceName);
+
+            // Add some claim, don't forget to add destination otherwise it won't be added to the access token.
+            identity.AddClaim("nimble", "framework", OpenIddictConstants.Destinations.AccessToken);
+
+            claimsPrincipal = new ClaimsPrincipal(identity);
+
+            claimsPrincipal.SetScopes(request.GetScopes());
+        }
+        else
+        {
+            throw new InvalidOperationException("The specified grant type is not supported.");
         }
 
-        //[HttpPost("~/connect/token")]
-        //[Consumes("application/x-www-form-urlencoded")]
-        //[Produces("application/json")]
-        public IResult Exchange(HttpContext httpContext, IOpenIddictScopeManager manager)
-        {
-            var request = httpContext.GetOpenIddictServerRequest() ??
-                          throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
-
-            ClaimsPrincipal claimsPrincipal;
-
-            if (request.IsClientCredentialsGrantType())
-            {
-                // Note: the client credentials are automatically validated by OpenIddict:
-                // if client_id or client_secret are invalid, this action won't be invoked.
-
-                var identity = new ClaimsIdentity(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-
-                // Subject (sub) is a required field, we use the client id as the subject identifier here.
-                identity.AddClaim(OpenIddictConstants.Claims.Subject, request.ClientId ?? throw new InvalidOperationException());
-
-                // Add some claim, don't forget to add destination otherwise it won't be added to the access token.
-                identity.AddClaim("some-claim", "some-value", OpenIddictConstants.Destinations.AccessToken);
-
-                
-
-                claimsPrincipal = new ClaimsPrincipal(identity);
-
-                claimsPrincipal.SetScopes(request.GetScopes());
-            }
-
-            else
-            {
-                throw new InvalidOperationException("The specified grant type is not supported.");
-            }
-
-            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
-            return Results.SignIn(claimsPrincipal,new AuthenticationProperties(), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-        }
-
-
-        //[HttpPost]
-        //[Produces("application/json")]
-        //private IResult Exchange(HttpContext httpContext, IOpenIddictScopeManager manager)
-        //{
-        //    var request = httpContext.GetOpenIddictServerRequest() ??
-        //                  throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
-
-        //    ClaimsPrincipal claimsPrincipal;
-
-        //    if (request.IsClientCredentialsGrantType())
-        //    {
-        //        // Note: the client credentials are automatically validated by OpenIddict:
-        //        // if client_id or client_secret are invalid, this action won't be invoked.
-
-        //        var identity = new ClaimsIdentity(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-
-        //        // Subject (sub) is a required field, we use the client id as the subject identifier here.
-        //        identity.AddClaim(OpenIddictConstants.Claims.Subject, request.ClientId ?? throw new InvalidOperationException());
-
-        //        // Add some claim, don't forget to add destination otherwise it won't be added to the access token.
-        //        identity.AddClaim("some-claim", "some-value", OpenIddictConstants.Destinations.AccessToken);
-
-        //        claimsPrincipal = new ClaimsPrincipal(identity);
-
-        //        claimsPrincipal.SetScopes(request.GetScopes());
-        //    }
-
-        //    else
-        //    {
-        //        throw new InvalidOperationException("The specified grant type is not supported.");
-        //    }
-
-        //    // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
-        //    return  Results.SignIn(claimsPrincipal, null, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-        //}
+        // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+        return Results.SignIn(claimsPrincipal, new AuthenticationProperties(), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
     }
 }
