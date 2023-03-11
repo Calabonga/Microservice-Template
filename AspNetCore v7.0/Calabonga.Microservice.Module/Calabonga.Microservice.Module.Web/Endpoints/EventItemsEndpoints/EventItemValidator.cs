f596da87a5af76b08d9713f238beee17030f103b
Calabonga <dev@calabonga.net>
2023-03-11 11:35:09 +0700

    Validation updated.

--- AspNetCore v7.0/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/Endpoints/EventItemsEndpoints/EventItemValidator.cs
+++ AspNetCore v7.0/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Web/Endpoints/EventItemsEndpoints/EventItemValidator.cs
@@ -1,4 +1,4 @@
-﻿using Calabonga.Microservice.Module.Web.Endpoints.EventItemsEndpoints.ViewModels;
+﻿using Calabonga.Microservice.Module.Web.Endpoints.EventItemsEndpoints.Queries;
 using FluentValidation;
 
 namespace Calabonga.Microservice.Module.Web.Endpoints.EventItemsEndpoints;
@@ -6,16 +6,15 @@ namespace Calabonga.Microservice.Module.Web.Endpoints.EventItemsEndpoints;
 /// <summary>
 /// RegisterViewModel Validator
 /// </summary>
-public class EventItemCreateRequestValidator : AbstractValidator<EventItemCreateViewModel>
+public class EventItemCreateRequestValidator : AbstractValidator<PostEventItemRequest>
 {
     public EventItemCreateRequestValidator() => RuleSet("default", () =>
     {
-        RuleFor(x => x.CreatedAt).NotNull();
-        RuleFor(x => x.Message).NotEmpty().NotNull().MaximumLength(4000);
-        RuleFor(x => x.Level).NotEmpty().NotNull().MaximumLength(50);
-        RuleFor(x => x.Logger).NotNull().NotEmpty().MaximumLength(255);
-
-        RuleFor(x => x.ThreadId).MaximumLength(50);
-        RuleFor(x => x.ExceptionMessage).MaximumLength(4000);
+        RuleFor(x => x.Model.CreatedAt).NotNull();
+        RuleFor(x => x.Model.Message).NotEmpty().MaximumLength(4000);
+        RuleFor(x => x.Model.Level).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Model.Logger).NotEmpty().MaximumLength(255);
+        RuleFor(x => x.Model.ThreadId).MaximumLength(50);
+        RuleFor(x => x.Model.ExceptionMessage).MaximumLength(4000);
     });
 }
