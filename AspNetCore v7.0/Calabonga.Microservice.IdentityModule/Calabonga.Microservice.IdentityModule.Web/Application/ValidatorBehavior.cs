2dae72f484e7595ebf9c6c61a9c818ef31b6fa83
Calabonga <dev@calabonga.net>
2023-03-18 09:52:38 +0700

    validation handler updated

--- AspNetCore v7.0/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Application/ValidatorBehavior.cs
+++ AspNetCore v7.0/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Application/ValidatorBehavior.cs
@@ -35,7 +35,14 @@ public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest
             return next();
         }
 
-        // return new ProblemDetails();
-        throw new ValidationException(failures);
+        var type = typeof(TResponse);
+        if (!type.IsSubclassOf(typeof(OperationResult)))
+        {
+            throw new ValidationException(failures);
+        }
+
+        var result = Activator.CreateInstance(type);
+        ((OperationResult)result!).AddError(new ValidationException(failures));
+        return Task.FromResult((TResponse)result!);
     }
 }
