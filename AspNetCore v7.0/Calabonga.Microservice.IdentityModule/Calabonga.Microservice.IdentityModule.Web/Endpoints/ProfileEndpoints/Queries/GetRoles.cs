5fff882af0470bf389170e736f9db86e0facff9c
Calabonga <dev@calabonga.net>
2023-04-15 09:31:34 +0700

    Update nugets versions and serilog settings

--- AspNetCore v7.0/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Endpoints/ProfileEndpoints/Queries/GetRoles.cs
+++ AspNetCore v7.0/Calabonga.Microservice.IdentityModule/Calabonga.Microservice.IdentityModule.Web/Endpoints/ProfileEndpoints/Queries/GetRoles.cs
@@ -12,13 +12,19 @@ public record GetRolesRequest : IRequest<string>;
 public class GetRolesRequestHandler : IRequestHandler<GetRolesRequest, string>
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ILogger<GetRolesRequestHandler> _logger;
 
-    public GetRolesRequestHandler(IHttpContextAccessor httpContextAccessor) => _httpContextAccessor = httpContextAccessor;
+    public GetRolesRequestHandler(IHttpContextAccessor httpContextAccessor, ILogger<GetRolesRequestHandler> logger)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
+    }
 
     public Task<string> Handle(GetRolesRequest request, CancellationToken cancellationToken)
     {
         var user = _httpContextAccessor.HttpContext!.User;
         var roles = ClaimsHelper.GetValues<string>((ClaimsIdentity)user.Identity!, "role");
+        _logger.LogInformation("Current user {@UserName} have following roles {@Roles}", user.Identity.Name, roles);
         return Task.FromResult($"Current user ({user.Identity!.Name}) have following roles: {string.Join("|", roles)}");
     }
 }
