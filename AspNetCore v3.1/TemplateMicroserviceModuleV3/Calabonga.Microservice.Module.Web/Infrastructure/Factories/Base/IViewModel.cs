7bd9e830d2d153f01a6cbe7759460e0819b4ddfa
Sergei Calabonga <s.calabonga@qwell.ru>
2024-11-26 09:23:30 +0700

    AspNetCore 3.1 removed

--- AspNetCore v3.1/TemplateMicroserviceModuleV3/Calabonga.Microservice.Module.Web/Infrastructure/Factories/Base/IViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace $safeprojectname$.Infrastructure.Factories.Base
-{
-    /// <summary>
-    /// Just a stub for <see cref="ViewModelFactory{TEntity,TCreateViewModel,TUpdateViewModel}"/>see
-    /// </summary>
-    public interface IViewModel { }
-}
