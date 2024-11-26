b7190131b94e83c141943044969ff4779ce37f76
Sergei Calabonga <s.calabonga@qwell.ru>
2024-11-26 09:23:53 +0700

    AspNetCore 6.0 removed

--- AspNetCore v6.0/Calabonga.Microservice.Module/Calabonga.Microservice.Module.Infrastructure/GlobalUsing.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Calabonga.Microservice.Module.Domain;
-global using Calabonga.Microservice.Module.Domain.Base;
-global using Microsoft.AspNetCore.Identity;
-global using Microsoft.Extensions.DependencyInjection;
